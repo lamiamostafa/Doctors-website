b198d75c5e0aab537ba931dec764dbf5483f9b06
lamiamostafa <lamiamostafa791@gmail.com>
2022-04-18 16:04:39 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Services from './Pages/Home/Services/Services';
 import Blogs from './Pages/Home/Blogs/Blogs';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 
 
@@ -20,6 +22,8 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/services" element={<Services></Services>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
 
       </Routes>
