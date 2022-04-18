f0ffff1abc344346fb2467b4a810b1b73ef47f87
lamiamostafa <lamiamostafa791@gmail.com>
2022-04-18 15:12:03 +0600

    NotFound page is added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Home from './Pages/Home/Home/Home';
 import Services from './Pages/Home/Services/Services';
 import Blogs from './Pages/Home/Blogs/Blogs';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 import Footer from './Pages/Shared/Footer/Footer';
+
 
 function App() {
   return (
@@ -14,9 +16,11 @@ function App() {
       <Routes>
 
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/services" element={<Services></Services>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
 
       </Routes>
       <Footer></Footer>
